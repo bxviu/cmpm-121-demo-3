8d81c625245a4060ee1a4c59de74aee553bf9be4
Benthan Vu <bxvu@ucsc.edu>
2023-11-04 14:25:46 -0700

    added ability to see coins and deposit by clicking on player marker and a pit together

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const PIT_SPAWN_PROBABILITY = 0.1;
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-  center: MERRILL_CLASSROOM,
+  center: NULL_ISLAND,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -40,8 +40,52 @@ leaflet
   })
   .addTo(map);
 
+let selectedPit: {
+  cache: GeoCoin[];
+  container: HTMLDivElement;
+  coordinate: { lat: number; lng: number };
+} | null = null;
 const playerMarker = leaflet.marker(NULL_ISLAND);
+const playerContainer = document.createElement("div");
+playerContainer.addEventListener("click", (e) => {
+  if (e.target instanceof HTMLButtonElement) {
+    const regex = /(-?\d+)/g;
+    const result = e.target.id.match(regex);
+
+    // Convert the matched strings to numbers
+    const numbers = result!.map((match) => parseInt(match, 10));
+
+    const [lat, lng, serial] = numbers;
+    totalCoins--;
+    // remove coin and add it to player inventory
+    const foundCoin = getCoin(playerCoins, {
+      lat: lat,
+      lng: lng,
+      serial: serial,
+    } as GeoCoin);
+    selectedPit!.cache.push(
+      playerCoins.splice(playerCoins.indexOf(foundCoin!), 1)[0]
+    );
+    statusPanel.innerHTML = `${totalCoins} total GeoCoin(s)`;
+    updateGeoCoinCache(
+      selectedPit!.cache,
+      selectedPit!.container,
+      selectedPit!.coordinate
+    );
+    updatePlayerCache(playerCoins, playerContainer);
+  }
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
+});
 playerMarker.bindTooltip("That's you!");
+playerMarker.bindPopup(
+  () => {
+    updatePlayerCache(playerCoins, playerContainer);
+    return playerContainer;
+  },
+  { closeOnClick: false, autoClose: false }
+);
 playerMarker.addTo(map);
 
 const sensorButton = document.querySelector("#sensor")!;
@@ -60,10 +104,6 @@ interface GeoCoin {
   serial: number;
 }
 
-// function createCoin() {
-
-// }
-
 function getCoin(coinCache: GeoCoin[], coinData: GeoCoin): GeoCoin | undefined {
   for (const coin of coinCache) {
     if (
@@ -71,7 +111,6 @@ function getCoin(coinCache: GeoCoin[], coinData: GeoCoin): GeoCoin | undefined {
       coin.lng == coinData.lng &&
       coin.serial == coinData.serial
     ) {
-      console.log("coin found");
       return coin;
     }
   }
@@ -81,7 +120,7 @@ function getCoin(coinCache: GeoCoin[], coinData: GeoCoin): GeoCoin | undefined {
 let totalCoins = 0;
 const playerCoins: GeoCoin[] = [];
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No totalCoins yet...";
+statusPanel.innerHTML = "No coins yet...";
 
 const worldMap = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 
@@ -121,12 +160,13 @@ function makePit(i: number, j: number, originLocation: leaflet.LatLng) {
   pit.bindPopup(() => {
     tip.closeTooltip();
     const container = document.createElement("div");
-    container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has <span id="coinAmount">${geoCoinCache.length}</span> coins.</div>`;
-    geoCoinCache.forEach((coin) => {
-      console.log(`GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} `);
-      container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} <button id="collectLat${coin.lat}Lng${coin.lng}S${coin.serial}">Collect</button></div>`;
-    });
+    selectedPit = {
+      cache: geoCoinCache,
+      container: container,
+      coordinate: coordinate,
+    };
+    updatePlayerCache(playerCoins, playerContainer);
+    updateGeoCoinCache(geoCoinCache, container, coordinate);
     container.addEventListener("click", (e) => {
       if (e.target instanceof HTMLButtonElement) {
         const regex = /(-?\d+)/g;
@@ -138,20 +178,17 @@ function makePit(i: number, j: number, originLocation: leaflet.LatLng) {
         const [lat, lng, serial] = numbers;
         totalCoins++;
         // remove coin and add it to player inventory
-        console.log(geoCoinCache);
         const foundCoin = getCoin(geoCoinCache, {
           lat: lat,
           lng: lng,
           serial: serial,
         } as GeoCoin);
-        console.log(foundCoin);
-        console.log(geoCoinCache.indexOf(foundCoin!));
         playerCoins.push(
           geoCoinCache.splice(geoCoinCache.indexOf(foundCoin!), 1)[0]
         );
         statusPanel.innerHTML = `${totalCoins} total GeoCoin(s)`;
-        console.log(geoCoinCache);
-        updateGeoCoinCache(geoCoinCache, container);
+        updateGeoCoinCache(geoCoinCache, container, coordinate);
+        updatePlayerCache(playerCoins, playerContainer);
       }
       e.stopPropagation();
       e.preventDefault();
@@ -159,18 +196,32 @@ function makePit(i: number, j: number, originLocation: leaflet.LatLng) {
     });
     return container;
   });
+  pit.getPopup()!.on("remove", () => {
+    selectedPit = null;
+    updatePlayerCache(playerCoins, playerContainer);
+  });
   pit.addTo(map);
 }
 
 function updateGeoCoinCache(
   geoCoinCache: GeoCoin[],
-  container: HTMLDivElement
+  container: HTMLDivElement,
+  coordinate: { lat: number; lng: number }
 ) {
-  container.innerHTML = `<div>There is a pit here at "${0},${0}". It has <span id="coinAmount">${
-    geoCoinCache.length
-  }</span> coins.</div>`;
+  container.innerHTML = `<div>There is a pit here at "${coordinate.lat}, ${coordinate.lng}". It has <span id="coinAmount">${geoCoinCache.length}</span> coins.</div>`;
   geoCoinCache.forEach((coin) => {
     container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} <button id="collectLat${coin.lat}Lng${coin.lng}S${coin.serial}">Collect</button></div>`;
+  });
+}
+
+function updatePlayerCache(coinCache: GeoCoin[], container: HTMLDivElement) {
+  container.innerHTML = `<div>You have <span id="coinAmount">${coinCache.length}</span> coins.</div>`;
+  coinCache.forEach((coin) => {
+    if (selectedPit) {
+      container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} <button id="depositLat${coin.lat}Lng${coin.lng}S${coin.serial}">Deposit</button></div>`;
+    } else {
+      container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial}</div>`;
+    }
   });
 }
 
@@ -191,7 +242,7 @@ function createCell(i: number, j: number, originLocation: leaflet.LatLng) {
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
     if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-      createCell(i, j, MERRILL_CLASSROOM);
+      createCell(i, j, NULL_ISLAND);
     }
   }
 }
