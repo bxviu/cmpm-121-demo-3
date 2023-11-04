c4e6cdb93242decbfd5d6f0fd8fd6700fbbeb81c
Benthan Vu <bxvu@ucsc.edu>
2023-11-04 13:46:07 -0700

    added unique coins and collecting them

---
+++ src/board.ts
@@ -0,0 +1,61 @@
+import leaflet from "leaflet";
+
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+export class Board {
+  readonly tileWidth: number;
+  readonly tileVisibilityRadius: number;
+
+  private readonly knownCells: Map<string, Cell>;
+
+  constructor(tileWidth: number, tileVisibilityRadius: number) {
+    this.tileWidth = tileWidth;
+    this.tileVisibilityRadius = tileVisibilityRadius;
+    this.knownCells = new Map();
+  }
+
+  private getCanonicalCell(cell: Cell): Cell | undefined {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+    return this.knownCells.get(key);
+  }
+
+  getCellForPoint(point: leaflet.LatLng): Cell | undefined {
+    return this.getCanonicalCell({
+      i: point.lat,
+      j: point.lng,
+    });
+  }
+
+  setCellForPoint(cell: Cell) {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+    this.knownCells.set(key, cell);
+  }
+
+  getCellBounds(cell: Cell): leaflet.LatLngBounds {
+    return leaflet.latLngBounds([
+      [cell.i, cell.j],
+      [cell.i + this.tileWidth, cell.j + this.tileWidth],
+    ]);
+  }
+
+  getCellsNearPoint(point: leaflet.LatLng): Cell[] {
+    const resultCells: Cell[] = [];
+    const originCell = this.getCellForPoint(point);
+    const originBounds = this.getCellBounds(originCell!);
+    this.knownCells.forEach((cell) => {
+      if (originCell!.i === cell.i && originCell!.j === cell.j) {
+        return;
+      }
+      const cellBounds = this.getCellBounds(cell);
+      if (originBounds.pad(this.tileVisibilityRadius).intersects(cellBounds)) {
+        resultCells.push(cell);
+      }
+    });
+    return resultCells;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
+import { Board } from "./board";
 
 const MERRILL_CLASSROOM = leaflet.latLng({
   lat: 36.9995,
   lng: -122.0533,
+});
+
+const NULL_ISLAND = leaflet.latLng({
+  lat: 0,
+  lng: 0,
 });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
@@ -34,7 +40,7 @@ leaflet
   })
   .addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+const playerMarker = leaflet.marker(NULL_ISLAND);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
@@ -48,64 +54,144 @@ sensorButton.addEventListener("click", () => {
   });
 });
 
-let points = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No points yet...";
+interface GeoCoin {
+  lat: number;
+  lng: number;
+  serial: number;
+}
 
-function makePit(i: number, j: number) {
+// function createCoin() {
+
+// }
+
+function getCoin(coinCache: GeoCoin[], coinData: GeoCoin): GeoCoin | undefined {
+  for (const coin of coinCache) {
+    if (
+      coin.lat == coinData.lat &&
+      coin.lng == coinData.lng &&
+      coin.serial == coinData.serial
+    ) {
+      console.log("coin found");
+      return coin;
+    }
+  }
+  return undefined;
+}
+
+let totalCoins = 0;
+const playerCoins: GeoCoin[] = [];
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No totalCoins yet...";
+
+const worldMap = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
+
+function makePit(i: number, j: number, originLocation: leaflet.LatLng) {
   const bounds = leaflet.latLngBounds([
     [
-      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+      originLocation.lat + i * TILE_DEGREES,
+      originLocation.lng + j * TILE_DEGREES,
     ],
     [
-      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+      originLocation.lat + (i + 1) * TILE_DEGREES,
+      originLocation.lng + (j + 1) * TILE_DEGREES,
     ],
   ]);
 
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-  let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
 
-  const tip = pit.bindTooltip(`Pit: ${i},${j}`);
+  const startingCoins = Math.floor(
+    Math.floor(luck([i, j, "initialValue"].toString()) * 100) / 15
+  );
+
+  const coordinate = {
+    lat: Math.floor(bounds.getCenter().lat / TILE_DEGREES),
+    lng: Math.floor(bounds.getCenter().lng / TILE_DEGREES),
+  };
+  const geoCoinCache: GeoCoin[] = [];
+  for (let k = 0; k < startingCoins; k++) {
+    geoCoinCache.push({
+      lat: coordinate.lat,
+      lng: coordinate.lng,
+      serial: k,
+    });
+  }
+
+  const tip = pit.bindTooltip(`Pit: ${coordinate.lat},${coordinate.lng}`);
+
   pit.bindPopup(() => {
     tip.closeTooltip();
     const container = document.createElement("div");
     container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="collect">collect</button>
-                <button id="deposit">deposit</button>`;
-    const collect = container.querySelector<HTMLButtonElement>("#collect")!;
-    collect.addEventListener("click", () => {
-      if (value <= 0) {
-        return;
-      }
-      value--;
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      points++;
-      statusPanel.innerHTML = `${points} points accumulated`;
+                <div>There is a pit here at "${i},${j}". It has <span id="coinAmount">${geoCoinCache.length}</span> coins.</div>`;
+    geoCoinCache.forEach((coin) => {
+      console.log(`GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} `);
+      container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} <button id="collectLat${coin.lat}Lng${coin.lng}S${coin.serial}">Collect</button></div>`;
     });
-    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
-    deposit.addEventListener("click", () => {
-      if (points <= 0) {
-        return;
+    container.addEventListener("click", (e) => {
+      if (e.target instanceof HTMLButtonElement) {
+        const regex = /(-?\d+)/g;
+        const result = e.target.id.match(regex);
+
+        // Convert the matched strings to numbers
+        const numbers = result!.map((match) => parseInt(match, 10));
+
+        const [lat, lng, serial] = numbers;
+        totalCoins++;
+        // remove coin and add it to player inventory
+        console.log(geoCoinCache);
+        const foundCoin = getCoin(geoCoinCache, {
+          lat: lat,
+          lng: lng,
+          serial: serial,
+        } as GeoCoin);
+        console.log(foundCoin);
+        console.log(geoCoinCache.indexOf(foundCoin!));
+        playerCoins.push(
+          geoCoinCache.splice(geoCoinCache.indexOf(foundCoin!), 1)[0]
+        );
+        statusPanel.innerHTML = `${totalCoins} total GeoCoin(s)`;
+        console.log(geoCoinCache);
+        updateGeoCoinCache(geoCoinCache, container);
       }
-      value++;
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      points--;
-      statusPanel.innerHTML = `${points} points accumulated`;
+      e.stopPropagation();
+      e.preventDefault();
+      return false;
     });
     return container;
   });
   pit.addTo(map);
 }
 
+function updateGeoCoinCache(
+  geoCoinCache: GeoCoin[],
+  container: HTMLDivElement
+) {
+  container.innerHTML = `<div>There is a pit here at "${0},${0}". It has <span id="coinAmount">${
+    geoCoinCache.length
+  }</span> coins.</div>`;
+  geoCoinCache.forEach((coin) => {
+    container.innerHTML += `<div>GeoCoin | ${coin.lat}:${coin.lng}#${coin.serial} <button id="collectLat${coin.lat}Lng${coin.lng}S${coin.serial}">Collect</button></div>`;
+  });
+}
+
+function createCell(i: number, j: number, originLocation: leaflet.LatLng) {
+  const pitCell = worldMap.getCellForPoint(
+    leaflet.latLng({
+      lat: originLocation.lat + i * TILE_DEGREES,
+      lng: originLocation.lng + j * TILE_DEGREES,
+    })
+  );
+  if (pitCell) {
+    return;
+  }
+  makePit(i, j, originLocation);
+  worldMap.setCellForPoint({ i: i, j: j });
+}
+
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
     if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-      makePit(i, j);
+      createCell(i, j, MERRILL_CLASSROOM);
     }
   }
 }
